60cf6034ae33738ae0901ae5c90628c11496853c
Florian Widder <florian.widder@live.de>
2019-10-20 13:20:34 +0200

    Add Project Skeleton

---
+++ src/test/java/com/github/dfprojects/SpringCloudStarter/SpringCloudStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dfprojects.SpringCloudStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
