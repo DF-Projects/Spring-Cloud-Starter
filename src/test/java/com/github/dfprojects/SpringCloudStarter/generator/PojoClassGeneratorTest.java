ce0af4e2ea3d526dcf6dd7dd0d346859675ef7bb
Florian Widder <florian.widder@live.de>
2019-10-20 20:14:06 +0200

    add PojoClassGenerator and Tests

---
+++ src/test/java/com/github/dfprojects/SpringCloudStarter/generator/PojoClassGeneratorTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.github.dfprojects.SpringCloudStarter.generator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.contentOf;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Tests für
+ * {@link com.github.dfprojects.SpringCloudStarter.generator.PojoClassGenerator
+ * PojoClassGenerator}
+ * 
+ * @author Florian Widder
+ *
+ */
+class PojoClassGeneratorTest {
+
+	/**
+	 * The
+	 * {@link com.github.dfprojects.SpringCloudStarter.generator.PojoClassGenerator
+	 * PojoClassGenerator} under Test
+	 */
+	PojoClassGenerator pojoClassGenerator = new PojoClassGenerator();
+
+	/**
+	 * Test method for
+	 * {@link com.github.dfprojects.SpringCloudStarter.generator.PojoClassGenerator#generate(java.lang.String, java.lang.String, java.util.Map)}.
+	 * 
+	 * @throws IOException if Test File is not found
+	 */
+	@Test
+	final void testGenerate() throws IOException {
+		String packageName = "org.packageTest.test";
+		String className = "TestClass";
+		Map<String, String> attributs = new HashMap<>();
+		attributs.put("intTest", "int");
+
+		String generatedClass = pojoClassGenerator.generate(packageName, className, attributs);
+
+		URL expURL = new ClassPathResource("/PojoClassGeneratorTest/TestClass.java").getURL();
+		assertThat(generatedClass).isEqualTo(contentOf(expURL));
+	}
+
+}
---
+++ src/test/resources/PojoClassGeneratorTest/TestClass.java
@@ -0,0 +1,14 @@
+package org.packageTest.test;
+
+public class TestClass {
+
+  private int intTest;
+
+  public int getintTest() {
+    return intTest;
+  }
+
+  public void getintTest(int intTest) {
+    this.intTest = intTest;
+  }
+}
